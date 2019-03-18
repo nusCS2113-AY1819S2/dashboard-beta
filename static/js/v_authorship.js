2d252e2901a308acaacffd53cefea6ca5b6fbba5
Deployment Bot (Travis) <deploy@travis-ci.org>
2019-03-18 10:46:37 +0000

    Rebuild pages at e551e0cb721cfefcf181d2a5e07683000cc71c51

--- static/js/v_authorship.js
+++ static/js/v_authorship.js
@@ -14,26 +14,6 @@ window.toggleNext = function toggleNext(ele) {
   }
 
   parent.className = classes.join(' ');
-
-  // Update expand/collapse all button
-  window.updateToggleButton();
-};
-
-window.updateToggleButton = function updateToggleButton() {
-  if (document.getElementsByClassName('file active').length === document.getElementsByClassName('file').length) {
-    window.app.isCollapsed = false;
-  } else if (document.getElementsByClassName('file active').length === 0) {
-    window.app.isCollapsed = true;
-  }
-};
-
-window.expandAll = function expandAll(isActive) {
-  const renameValue = isActive ? 'file active' : 'file';
-
-  const files = document.getElementsByClassName('file');
-  Array.from(files).forEach((file) => {
-    file.className = renameValue;
-  });
 };
 
 const repoCache = [];
@@ -47,12 +27,12 @@ window.vAuthorship = {
       isLoaded: false,
       files: [],
       isSelectAllChecked: true,
-      selectedFileTypes: [],
-      fileTypes: [],
-      filesLinesObj: {},
+      selectedFileFormats: [],
+      fileFormats: [],
       filesBlankLinesObj: {},
       totalLineCount: '',
       totalBlankLineCount: '',
+      activeFilesCount: 0,
       filterSearch: '*',
     };
   },
@@ -61,6 +41,11 @@ window.vAuthorship = {
     initiate() {
       const repo = window.REPOS[this.info.repo];
 
+      this.getRepoProps(repo);
+      if (!repo || !this.info.name) {
+        window.app.isTabActive = false;
+        return;
+      }
       if (repoCache.length === 2) {
         const toRemove = repoCache.shift();
         if (toRemove !== this.info.repo) {
@@ -75,6 +60,46 @@ window.vAuthorship = {
         window.api.loadAuthorship(this.info.repo)
             .then((files) => this.processFiles(files));
       }
+    },
+
+    getRepoProps(repo) {
+      if (repo) {
+        const author = repo.users.filter((user) => user.name === this.info.author);
+        if (author.length > 0) {
+          this.info.name = author[0].displayName;
+          this.filesLinesObj = author[0].fileFormatContribution;
+        }
+        this.info.location = repo.location.location;
+      }
+    },
+
+    setInfoHash() {
+      const { addHash, removeHash } = window;
+      addHash('tabAuthor', this.info.author);
+      addHash('tabRepo', this.info.repo);
+      removeHash('tabOpen');
+    },
+
+    expandAll(isActive) {
+      const renameValue = isActive ? 'file active' : 'file';
+
+      const files = document.getElementsByClassName('file');
+      Array.from(files).forEach((file) => {
+        file.className = renameValue;
+      });
+
+      this.activeFilesCount = isActive ? this.selectedFiles.length : 0;
+    },
+
+    updateCount() {
+      this.activeFilesCount = document.getElementsByClassName('file active').length;
+    },
+
+    hasCommits(info) {
+      if (window.REPOS[info.repo]) {
+        return window.REPOS[info.repo].commits.authorFinalContributionMap[info.author] > 0;
+      }
+      return false;
     },
 
     splitSegments(lines) {
@@ -113,7 +138,6 @@ window.vAuthorship = {
 
     processFiles(files) {
       const res = [];
-      const filesInfoObj = {};
       const filesBlanksInfoObj = {};
       let totalLineCount = 0;
       let totalBlankLineCount = 0;
@@ -125,12 +149,12 @@ window.vAuthorship = {
           const out = {};
           out.path = file.path;
           out.lineCount = lineCnt;
-          this.addLineCountToFileType(file.path, lineCnt, filesInfoObj);
 
           const segmentInfo = this.splitSegments(file.lines);
           out.segments = segmentInfo.segments;
           totalBlankLineCount += segmentInfo.blankLineCount;
-          this.addLineCountToFileType(file.path, segmentInfo.blankLineCount, filesBlanksInfoObj);
+          this.addBlankLineCountToFileFormat(file.path, segmentInfo.blankLineCount,
+              filesBlanksInfoObj);
           res.push(out);
         }
       });
@@ -139,57 +163,56 @@ window.vAuthorship = {
       this.totalBlankLineCount = totalBlankLineCount;
       res.sort((a, b) => b.lineCount - a.lineCount);
 
-      this.filesLinesObj = this.sortFileTypeAlphabetically(filesInfoObj);
-      Object.keys(filesInfoObj).forEach((file) => {
-        this.selectedFileTypes.push(file);
-        this.fileTypes.push(file);
+      Object.keys(this.filesLinesObj).forEach((file) => {
+        this.selectedFileFormats.push(file);
+        this.fileFormats.push(file);
       });
 
       this.filesBlankLinesObj = filesBlanksInfoObj;
       this.files = res;
       this.isLoaded = true;
+
+      this.activeFilesCount = this.selectedFiles.length;
     },
 
-    addLineCountToFileType(path, lineCount, filesInfoObj) {
-      let fileType = path.split('.').pop();
-      fileType = (fileType.length === 0) ? 'others' : fileType;
+    addBlankLineCountToFileFormat(path, lineCount, filesInfoObj) {
+      let fileFormat = path.split('.').pop();
+      fileFormat = (fileFormat.length === 0) ? 'others' : fileFormat;
 
-      if (!filesInfoObj[fileType]) {
-        filesInfoObj[fileType] = 0;
+      if (!filesInfoObj[fileFormat]) {
+        filesInfoObj[fileFormat] = 0;
       }
 
-      filesInfoObj[fileType] += lineCount;
-    },
-
-    sortFileTypeAlphabetically(unsortedFilesInfoObj) {
-      return Object.keys(unsortedFilesInfoObj)
-          .sort()
-          .reduce((acc, key) => ({
-            ...acc, [key]: unsortedFilesInfoObj[key],
-          }), {});
+      filesInfoObj[fileFormat] += lineCount;
     },
 
     selectAll() {
       if (!this.isSelectAllChecked) {
-        this.selectedFileTypes = this.fileTypes.slice();
+        this.selectedFileFormats = this.fileFormats.slice();
+        this.activeFilesCount = this.files.length;
       } else {
-        this.selectedFileTypes = [];
+        this.selectedFileFormats = [];
+        this.activeFilesCount = 0;
       }
     },
 
-    selectFileType(type) {
-      if (this.selectedFileTypes.includes(type)) {
-        const index = this.selectedFileTypes.indexOf(type);
-        this.selectedFileTypes.splice(index, 1);
+    selectFileFormat(format) {
+      if (this.selectedFileFormats.includes(format)) {
+        const index = this.selectedFileFormats.indexOf(format);
+        this.selectedFileFormats.splice(index, 1);
       } else {
-        this.selectedFileTypes.push(type);
+        this.selectedFileFormats.push(format);
       }
+    },
 
-      if (this.fileTypes.length === this.selectedFileTypes.length) {
+    getSelectedFiles() {
+      if (this.fileFormats.length === this.selectedFileFormats.length) {
         this.isSelectAllChecked = true;
-      } else if (this.selectedFileTypes.length === 0) {
+      } else if (this.selectedFileFormats.length === 0) {
         this.isSelectAllChecked = false;
       }
+
+      setTimeout(this.updateCount, 0);
     },
 
     updateFilterSearch(evt) {
@@ -197,7 +220,7 @@ window.vAuthorship = {
     },
 
     tickAllCheckboxes() {
-      this.selectedFileTypes = this.fileTypes.slice();
+      this.selectedFileFormats = this.fileFormats.slice();
       this.isSelectAllChecked = true;
       this.filterSearch = '*';
     },
@@ -209,7 +232,7 @@ window.vAuthorship = {
       submitButton.disabled = false;
 
       this.tickAllCheckboxes();
-      const checkboxes = document.getElementsByClassName('mui-checkbox--filetype');
+      const checkboxes = document.getElementsByClassName('mui-checkbox--fileformat');
       Array.from(checkboxes).forEach((checkbox) => {
         checkbox.disabled = true;
       });
@@ -223,7 +246,7 @@ window.vAuthorship = {
       submitButton.disabled = true;
 
       this.tickAllCheckboxes();
-      const checkboxes = document.getElementsByClassName('mui-checkbox--filetype');
+      const checkboxes = document.getElementsByClassName('mui-checkbox--fileformat');
       Array.from(checkboxes).forEach((checkbox) => {
         checkbox.disabled = false;
       });
@@ -231,7 +254,7 @@ window.vAuthorship = {
 
     isSelected(filePath) {
       const fileExt = filePath.split('.').pop();
-      return this.selectedFileTypes.includes(fileExt);
+      return this.selectedFileFormats.includes(fileExt);
     },
 
     getFileLink(file, path) {
@@ -241,10 +264,10 @@ window.vAuthorship = {
         repo.location.organization}/${repo.location.repoName}/${path}/${repo.branch}/${file.path}`;
     },
 
-    getFileBlankLineInfo(fileType) {
-      return `${fileType}: Blank: ${
-        this.filesBlankLinesObj[fileType]}, Non-Blank: ${
-        this.filesLinesObj[fileType] - this.filesBlankLinesObj[fileType]}`;
+    getFileBlankLineInfo(fileFormat) {
+      return `${fileFormat}: Blank: ${
+        this.filesBlankLinesObj[fileFormat]}, Non-Blank: ${
+        this.filesLinesObj[fileFormat] - this.filesBlankLinesObj[fileFormat]}`;
     },
 
     getTotalFileBlankLineInfo() {
@@ -258,14 +281,18 @@ window.vAuthorship = {
       return this.files.filter((file) => this.isSelected(file.path)
           && minimatch(file.path, this.filterSearch, { matchBase: true }));
     },
+    getExistingLinesObj() {
+      return Object.keys(this.filesLinesObj)
+          .filter((type) => this.filesLinesObj[type] > 0)
+          .reduce((acc, key) => ({
+            ...acc, [key]: this.filesLinesObj[key],
+          }), {});
+    },
   },
 
   created() {
     this.initiate();
-  },
-
-  updated() {
-    window.updateToggleButton();
+    this.setInfoHash();
   },
 };
 
